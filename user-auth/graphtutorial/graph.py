5611cd7d1aa0f941225dc193ba92033b07d1b519
Jason Johnston <jasonjoh@users.noreply.github.com>
2023-07-24 09:53:21 -0400

    Simplified initialization of GraphServiceClient

--- user-auth/graphtutorial/graph.py
+++ user-auth/graphtutorial/graph.py
@@ -4,13 +4,11 @@
 # <UserAuthConfigSnippet>
 from configparser import SectionProxy
 from azure.identity import DeviceCodeCredential
-from kiota_authentication_azure.azure_identity_authentication_provider import (
-    AzureIdentityAuthenticationProvider)
-from msgraph import GraphRequestAdapter, GraphServiceClient
-from msgraph.generated.me.me_request_builder import MeRequestBuilder
-from msgraph.generated.me.mail_folders.item.messages.messages_request_builder import (
+from msgraph import GraphServiceClient
+from msgraph.generated.users.item.user_item_request_builder import UserItemRequestBuilder
+from msgraph.generated.users.item.mail_folders.item.messages.messages_request_builder import (
     MessagesRequestBuilder)
-from msgraph.generated.me.send_mail.send_mail_post_request_body import SendMailPostRequestBody
+from msgraph.generated.users.item.send_mail.send_mail_post_request_body import SendMailPostRequestBody
 from msgraph.generated.models.message import Message
 from msgraph.generated.models.item_body import ItemBody
 from msgraph.generated.models.body_type import BodyType
@@ -20,7 +18,6 @@ from msgraph.generated.models.email_address import EmailAddress
 class Graph:
     settings: SectionProxy
     device_code_credential: DeviceCodeCredential
-    adapter: GraphRequestAdapter
     user_client: GraphServiceClient
 
     def __init__(self, config: SectionProxy):
@@ -30,11 +27,7 @@ class Graph:
         graph_scopes = self.settings['graphUserScopes'].split(' ')
 
         self.device_code_credential = DeviceCodeCredential(client_id, tenant_id = tenant_id)
-        auth_provider = AzureIdentityAuthenticationProvider(
-            self.device_code_credential,
-            scopes=graph_scopes)
-        self.adapter = GraphRequestAdapter(auth_provider)
-        self.user_client = GraphServiceClient(self.adapter)
+        self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
 # </UserAuthConfigSnippet>
 
     # <GetUserTokenSnippet>
@@ -47,10 +40,11 @@ class Graph:
     # <GetUserSnippet>
     async def get_user(self):
         # Only request specific properties using $select
-        query_params = MeRequestBuilder.MeRequestBuilderGetQueryParameters(
+        query_params = UserItemRequestBuilder.UserItemRequestBuilderGetQueryParameters(
             select=['displayName', 'mail', 'userPrincipalName']
         )
-        request_config = MeRequestBuilder.MeRequestBuilderGetRequestConfiguration(
+
+        request_config = UserItemRequestBuilder.UserItemRequestBuilderGetRequestConfiguration(
             query_parameters=query_params
         )
 
